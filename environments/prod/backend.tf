999f48734cb67731cd87c6b559ca5d50842bfb62
nickcube2 <awuninicholas@gmail.com>
2026-05-02 23:51:55 -0600

    fix: remove profile from backend config for ci compatibility

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -6,6 +6,5 @@ terraform {
     dynamodb_table = "cloud-platform-terraform-locks"
     encrypt        = true
     kms_key_id     = "arn:aws:kms:us-east-1:405449137534:key/219e066b-2285-4d37-9106-17e6795b0942"
-    profile        = "cloud-platform"
   }
 }
